aaf3d4de8fcd81e076afbd128d6f433b7d258cd4
Daniel <ivan17cf@gmail.com>
2021-05-20 12:28:26 -0500

    Creando entorno
    Configuraciones y más.

---
+++ test/test_lista.js
@@ -0,0 +1,19 @@
+/**
+ * En una lista vacia hay cero elementos.
+ * Cuando se agrega un elemento a una lista vacia hay un elemento.
+ * En una lista vacia no se encuentra ninguna clave.
+ * Cuando se agrega un elemento a una lista vacia se puede recuperar el valor a partir de la clave.
+ * Cuando se agrega una clave que ya esta en la lista se actualiza el valor correspondiente.
+ * Cuando se agrega un elemento a una lista vacia la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al final la lista de claves esta ordenada.
+ * 
+ */
+
+const assert = require("chai").assert;
+
+describe("en una lista vacia", function() {
+    it("Hay cero elementos", function() {
+        assert.fail("empezamos");
+    })
+})
