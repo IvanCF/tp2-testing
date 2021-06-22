cc4343369cc5f2f5fd6749247d75f48d6a62d06e
Daniel <ivan17cf@gmail.com>
2021-06-22 03:00:42 -0500

    Desarrollo Completo
    Test mejorados

--- src/Lista.js
+++ src/Lista.js
@@ -1,11 +1,115 @@
 module.exports = class Lista {
 
 
+
     constructor() {
+        this.elementos = [];
 
     }
+
     count() {
-        return 0;
+        return this.elementos.length;
     }
+
+    find() {
+        if (this.elementos.length == 1) {
+            return this.elementos[0].valor;
+        }
+        return NaN;
+    }
+
+    add(clave, valor) {
+        this.elementos.push({ clave, valor });
+        //console.log(this.elementos);
+    }
+
+    upd(clave, valor) {
+
+
+        var lst = this.elementos;
+        for (var i = 0; i < lst.length; i++) {
+            if (lst[i].clave == clave) {
+                this.elementos[i].valor = valor;
+                return;
+            }
+        }
+
+    }
+
+    addCabeza(clave, valor) {
+
+        //guardamos el primero actual
+        var item_inicio_key = this.elementos[0].clave;
+        var item_inicio_val = this.elementos[0].valor;
+
+        //agregamos el nuevo en la cabeza de la lista
+        this.elementos[0].clave = clave;
+        this.elementos[0].valor = valor;
+
+        //guardamos el anterior al actual cabeza
+        this.elementos.push({ clave, valor }); //reservamos el espacio temporal
+        var len = this.elementos.length;
+        //actualizamos el anterior desalojado de la cabeza
+        this.elementos[len - 1].clave = item_inicio_key;
+        this.elementos[len - 1].valor = item_inicio_val;
+
+
+    }
+
+    getCabeza() {
+
+        if (this.getSort()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    addCola(clave, valor) {
+        this.elementos.push({ clave, valor });
+    }
+
+    getCola() {
+        if (this.getSort()) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    del(clave) {
+        var lst = this.elementos;
+        for (var i = 0; i < lst.length; i++) {
+            if (lst[i].clave == clave) {
+                lst.splice(i, 1);
+                return;
+            }
+        }
+    }
+
+
+    getSort() {
+
+        this.elementos.sort(this.compare("clave")); //ordena la lista
+        console.log(this.elementos); //muestra la lista ordenada
+
+        return true;
+    }
+
+    compare(prop) {
+        return function(a, b) {
+            if (a[prop] < b[prop]) {
+
+                return -1;
+            }
+            if (a[prop] > b[prop]) {
+
+
+                return 1;
+            }
+            return 0;
+        };
+    }
+
 
 }
--- test/test_lista.js
+++ test/test_lista.js
@@ -1,12 +1,12 @@
 /**
- * En una lista vacia hay cero elementos.
- * Cuando se agrega un elemento a una lista vacia hay un elemento.
- * En una lista vacia no se encuentra ninguna clave.
- * Cuando se agrega un elemento a una lista vacia se puede recuperar el valor a partir de la clave.
- * Cuando se agrega una clave que ya esta en la lista se actualiza el valor correspondiente.
- * Cuando se agrega un elemento a una lista vacia la lista de claves esta ordenada.
- * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
- * Cuando se agrega un elemento al final la lista de claves esta ordenada.
+ * test 01: En una lista vacia hay cero elementos.
+ * test 02: Cuando se agrega un elemento a una lista vacia hay un elemento.
+ * test 03: En una lista vacia no se encuentra ninguna clave.
+ * test 04: Cuando se agrega un elemento a una lista vacia se puede recuperar el valor a partir de la clave.
+ * test 05: Cuando se agrega una clave que ya esta en la lista se actualiza el valor correspondiente.
+ * test 06: Cuando se agrega un elemento a una lista vacia la lista de claves esta ordenada.
+ * test 07: Cuando se agrega un elemento al principio la lista de claves esta ordenada.
+ * test 08: Cuando se agrega un elemento al final la lista de claves esta ordenada.
  * 
  */
 
@@ -14,9 +14,96 @@ const assert = require("chai").assert;
 
 const Lista = require("../src/Lista.js");
 
+
 describe("en una lista vacia", function() {
+
     var lista = new Lista();
+
+    //******************** test 01 ****************************/
     it("Hay cero elementos", function() {
         assert.equal(lista.count(), 0);
     })
+
+
+    //******************** test 03 ****************************/
+    it("No se encuentra ninguna clave", function() {
+        assert.isNaN(lista.find("clave"));
+    })
+
+
+    describe("cuado se agrega un elemento a una lista vacía", function() {
+        var lista = new Lista();
+        lista.add("clave", "valor");
+
+        //******************** test 02 ***************************/
+        it("hay un elemento", function() {
+            assert.equal(lista.count(), 1);
+        })
+    })
+
+
+    describe("Cuando se recupera el contenido de la lista", function() {
+        var lista = new Lista();
+        lista.add("clave", "valor");
+
+        it("hay un elemento", function() {
+                assert.equal(lista.count(), 1);
+            })
+            //******************** test 04 ****************************/
+        it("existe el elemento", function() {
+            assert.equal(lista.find("clave"), "valor");
+        })
+
+        //******************** test 06 ****************************/
+        it("la lista está ordenada por clave", function() {
+            lista.add("alave", "valor"); //agregamos un elemento para verificar si esta ordenando
+            assert.equal(lista.getSort(), true); //tener mas de 1 elemento ayudara a saber si esta ordenando.
+        })
+
+    })
+
+    describe("cuando se modifica el valor de un elemento de  la lista", function() {
+        var lista = new Lista();
+        lista.add("clave", "valor");
+        lista.upd("clave", "nuevo_valor");
+
+        //******************** test 05 ****************************/
+        it("se modifico el elemento", function() {
+            assert.equal(lista.find("clave"), "nuevo_valor");
+        })
+    })
+
+    describe("cuado se borra un elemento de la lista ", function() {
+        var lista = new Lista();
+        lista.del("clave");
+
+        it("no existe el elemento", function() {
+            assert.isNaN(lista.find("clave"));
+        })
+
+    })
+
+    describe("cuado agrega un elemento de la lista (Inicio / Final)", function() {
+        var lista = new Lista();
+        //agregamos 3 elementos para probar
+        lista.add("dlave", "valor1");
+        lista.add("alave", "valor2");
+        lista.add("xlave", "valor3");
+
+        //**************** test 07 *******************/
+        //se agrega en la cabeza y se volvera a ordenar
+        lista.addCabeza("clave", "valor");
+        it("Cuando se agrega al inicio de la lista", function() {
+            assert.equal(lista.getCabeza(), true);
+        })
+
+        //**************** test 08 *******************/
+        //se agrega en la cola y se vuelve a ordenar
+        lista.addCola("clavef", "valorf"); //agregamos un elemento para verificar si esta ordenando
+        it("Cuando se agrega al final de la lista", function() {
+            assert.equal(lista.getCola(), true); //tener mas de 1 elemento ayudará a saber si esta ordenando.
+        })
+
+    })
+
 })
